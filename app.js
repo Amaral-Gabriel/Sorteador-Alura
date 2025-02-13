0e6c6a5533557299821618bccf61cf8950c7dcba
Amaral-Gabriel <142606387+Amaral-Gabriel@users.noreply.github.com>
2025-02-13 14:20:45 -0300

    Adiciona função que verifica numero repetido

--- app.js
+++ app.js
@@ -1,26 +1,36 @@
-numerosSorteados = [];
 
+let numerosSorteados = [];
 function sortear() {
     let quantidadeNumero = parseInt(document.getElementById("quantidade").value);
     let primeiroNumero = parseInt(document.getElementById("de").value);
     let ultimoNumero = parseInt(document.getElementById("ate").value);
-    sorteiaNumero(quantidadeNumero, primeiroNumero, ultimoNumero)
-    document.querySelector('#resultado .texto__paragrafo').textContent = `Números sorteados: ${numerosSorteados}`;
+    for (var i = 0; i < quantidadeNumero; i++) {
+        sorteiaNumero(primeiroNumero, ultimoNumero)
+    }
+    document.querySelector('#resultado .texto__paragrafo').textContent = `Números sorteados: ${numerosSorteados.join(", ")}.`;
 }
 
-function sorteiaNumero(vezes, minimo, maximo) {
-    for (var i = 0; i < vezes; i++) {
-        let numeroAleatorio = parseInt(Math.random() * (maximo - minimo + 1) + minimo);
-        if (numerosSorteados.length <= maximo) {
-            if (numerosSorteados.includes(numeroAleatorio)){
-                sorteiaNumero(vezes, minimo, maximo);
-                return numeroAleatorio;
-            } else {
-                numerosSorteados.push(numeroAleatorio);
-                
-            }
-        }
-    } 
+function verificaRepetido(num) {
+    let primeiroNumero = parseInt(document.getElementById("de").value);
+    let ultimoNumero = parseInt(document.getElementById("ate").value);
+    
+    if (numerosSorteados.includes(num)){
+        sorteiaNumero(primeiroNumero, ultimoNumero);
+    } else {
+        numerosSorteados.push(num);
+    }
+    return numerosSorteados;
+}
+
+function sorteiaNumero(minimo, maximo) {
+    
+    let numeroAleatorio = parseInt(Math.random() * (maximo - minimo + 1) + minimo);
+    if (numerosSorteados.length <= maximo) {
+        verificaRepetido(numeroAleatorio);
+    } else {
+        numerosSorteados = [];
+        return numerosSorteados;
+    }
     return numerosSorteados;   
 }
 
